14bf66173a08f2874551e0ec6a80e04f7a6969fd
willcodes <difruscio.dev@gmail.com>
2017-02-18 12:28:50 -0500

    fixed post request to handle dynamically

--- server.js
+++ server.js
@@ -3,35 +3,25 @@
 //create your own config.js file that exports an object with your API key and BOT name
 
 const config = require('./config'),
-      express = require('express'),
-      app = express(),
-      path = require('path'),
-      fs = require('fs'),
-      bodyParser = require('body-parser'),
-      https = require('https'),
-      SlackBot = require('slackbots'),
-      request = require("request"),
-      fetch = require('node-fetch'),
-      port = process.env.PORT || 3001;
-      
+    express = require('express'),
+    app = express(),
+    path = require('path'),
+    fs = require('fs'),
+    bodyParser = require('body-parser'),
+    https = require('https'),
+    SlackBot = require('slackbots'),
+    request = require("request"),
+    fetch = require('node-fetch'),
+    port = process.env.PORT || 3001;
+
 const bot = new SlackBot({
     token: config.API_KEY,
     name: config.BOT_NAME
 });
 
-let getQuotes = (url) => {
-
-    let result = '';
-    
-    fetch(url)
-    .then(res => res.text())
-    .then(body => result = body);
-
-    return result;
-};
 
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 app.use(bodyParser.json());
@@ -43,15 +33,22 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.post('/getQuote', (req,res) => {
-    let text = req.text;
+app.post('/getQuote', (request, result) => {
 
-    res.send(getQuotes('http://seinfeld-api.willcodes.co/random'));
+    let text = request.text;
+    let theQuote = '';
 
-    // switch(text) {
-    //     case '':
-    //     getQuotes('http://seinfeld-api.willcodes.co/random');
-    // }
+    if (text === null || text === undefined || text === '') {
+        text = 'random';
+    }
+
+    fetch('http://seinfeld-api.willcodes.co/' + text)
+        .then(res => res.json())
+        .then(body => {
+            theQuote = body.quote + ' --' + body.author
+            result.send(theQuote);
+        });
+
 });
 
 
@@ -59,12 +56,12 @@ app.post('/getQuote', (req,res) => {
 
 
 //slackbot messaging
-bot.on('message', function(data) {
+bot.on('message', function (data) {
     // all ongoing events 
-    console.log(data); 
+    console.log(data);
 
 });
 
 app.listen(port, function () {
-  console.log('Server running on port ' + port);
+    console.log('Server running on port ' + port);
 });
